0700f16a63c5bc61ba8c81101e3a363546b187de
Peter Jones <pjones@devalot.com>
2024-09-10 16:08:42 -0700

    gpg-new-key: Make it clear what the password prompts are for

--- bin/gpg-new-key.sh
+++ bin/gpg-new-key.sh
@@ -8,6 +8,7 @@ set -o pipefail
 option_algo="ed25519/cert,sign+cv25519/encr"
 option_years=2
 option_email=()
+option_interactive=1
 
 ################################################################################
 gpg_options=("--batch" "--yes")
@@ -28,12 +29,26 @@ EOF
 }
 
 ################################################################################
+prompt() {
+  for line in "$@"; do
+    echo >&2 "==> $line"
+  done
+
+  if [ "$option_interactive" -eq 1 ]; then
+    echo >&2 "Press ENTER to continue..."
+    read -r
+  fi
+}
+
+################################################################################
 make_primary_key() {
   local primary_email="${option_email[0]}"
   local fp
 
   # Remove the primary email address from the list.
   unset 'option_email[0]'
+
+  prompt "Creating primary key.  Choose a passphrase for the key."
 
   gpg \
     "${gpg_options[@]}" \
@@ -48,6 +63,10 @@ make_primary_key() {
 
   if [ "${#option_email[@]}" -gt 0 ]; then
     for email in "${option_email[@]}"; do
+      prompt \
+        "Adding secondary address: $email." \
+        "You'll need to unlock the primary key."
+
       gpg --quick-add-uid "$primary_email" "$email"
     done
   fi
@@ -65,6 +84,10 @@ make_sub_keys() {
   expire=$(date --date="$option_years years" +%Y-%m-%d)
 
   for usage in "${usages[@]}"; do
+    prompt \
+      "Creating subkey \"$usage\"." \
+      "You'll need to unlock the primary key."
+
     gpg \
       "${gpg_options[@]}" \
       --quick-add-key \
@@ -90,10 +113,27 @@ backup_keys() {
     --output "$GNUPGHOME/../backup/$today-public.txt" \
     --export "$fingerprint"
 
+  prompt \
+    "Backup up secret subkeys." \
+    "You'll need to unlock the primary key."
+
   gpg \
     --armor \
     --export-secret-subkeys "$fingerprint" \
     >"$GNUPGHOME/../backup/$today-subkeys.txt"
+
+  # Make it easy to transfer the public key:
+  if [ -d "$GNUPGHOME/../../public" ]; then
+    cp \
+      "$GNUPGHOME/../backup/$today-public.txt" \
+      "$GNUPGHOME/../../public/public.txt"
+
+    cp -r \
+      "$GNUPGHOME/openpgp-revocs.d" \
+      "$GNUPGHOME/../../public/$today-openpgp-revocs.d"
+
+    echo "==> Public key stored in $(realpath "$GNUPGHOME/../../public")"
+  fi
 }
 
 ################################################################################
@@ -118,6 +158,7 @@ main() {
 
     t)
       set -x
+      option_interactive=0
       gpg_options+=(
         "--pinentry-mode" "loopback"
         "--passphrase" ""
@@ -148,10 +189,18 @@ main() {
   make_sub_keys "$fingerprint"
   backup_keys "$fingerprint"
 
-  cat <<DONE
+  cat >&2 <<DONE
 ==> Done!
 
 Your new key's fingerprint is $fingerprint.
+
+Now you need to upload your keys to a smartcard using:
+
+    gpg --edit-key $fingerprint
+
+Select keys with the 'key <number>' command and upload a key with the
+'keytocard' command.
+
 DONE
 }
 
--- test/gpg-new-key.sh
+++ test/gpg-new-key.sh
@@ -4,11 +4,14 @@ set -eux
 set -o pipefail
 
 export GNUPGHOME=/mnt/keys/gnupg
+public="$(dirname "$(dirname "$GNUPGHOME")")/public"
+
+mkdir -p "$public"
 mkdir -p "$(dirname "$GNUPGHOME")"
 mkdir -m 0700 "$GNUPGHOME"
 
 gpg-new-key.sh -t \
-  -e 'Joe T Foo <foo@example.com>' \
+  -e 'Joe T. Foo <foo@example.com>' \
   -e bar@example.com
 
 # Will fail if key doesn't exist:
@@ -23,3 +26,4 @@ test "$num" -eq 4
 test -d "$GNUPGHOME/../backup"
 test "$(find "$GNUPGHOME/../backup" -type f | wc -l)" -eq 3
 test -s "$GNUPGHOME/../backup/$(date +%Y-%m-%d)-subkeys.txt"
+test -e "$public/public.txt"
